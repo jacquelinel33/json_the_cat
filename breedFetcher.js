95f4253296f43932274d60868e5e00814a3319aa
Jacqueline <jlee4332@gmail.com>
2021-03-11 19:50:22 -0500

    adding tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,15 @@
 const request = require('request');
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = function (breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    callback(error, breed.description);
+  });
+};
 
-
-//take in url and return the description portion of the breed
-request(url, (error, response, body) => {
-  if (error) {
-    return console.log("error:", error);
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`This ${breedName} does not exist.`);
-  }
-});
-
-
-
-// findByBreed(url);
+module.exports = { fetchBreedDescription };
